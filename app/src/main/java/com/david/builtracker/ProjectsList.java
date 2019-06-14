a83ef1268fcab827791b263e4897b3f7312097ed
David mochoge <kingkgdave@gmail.com>
2019-06-14 09:19:44 +0300

    Commit changes

--- app/src/main/java/com/david/builtracker/ProjectsList.java
+++ app/src/main/java/com/david/builtracker/ProjectsList.java
@@ -3,11 +3,11 @@ package com.david.builtracker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ProjectsList extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_projects_list);
     }
 }
